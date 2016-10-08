1712da8ce3388a1b813c09c8317d8d8a6373723d
cacad101 <gbbpentium@gmail.com>
2016-10-08 21:21:37 +0800

    refine unit

--- Unit.py
+++ Unit.py
@@ -1,9 +1,11 @@
 # Unit class
 
 class Unit:
-    def __init__(self, unitID, playerID):
+    def __init__(self, unitID, playerID, rPos, cPos):
         self.unitID = unitID
         self.playerID = playerID
+        self.cPos = cPos
+        self.rPos = rPos
 
     def GetUnitID(self):
         return self.unitID
@@ -11,8 +13,20 @@ class Unit:
     def GetPlayerID(self):
         return self.playerID
 
+    def GetCol(self):
+        return self.cPos
+
+    def GetRow(self):
+        return self.rPos
+
     def SetUnitID(Self, unitID):
         self.unitID = unitID
 
     def SetPlayerID(self, playerID):
         self.playerID = playerID
+
+    def SetCol(self, cPos):
+        self.cPos = cPos
+
+    def SetRow(self, rPos):
+        self.rPos = rPos
