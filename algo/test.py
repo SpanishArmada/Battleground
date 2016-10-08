454524d8dfe4710073c3ab7bc6995194dc9d39b1
Dian Bakti <dbakti1605@gmail.com>
2016-10-09 01:50:00 +0800

    added 2 naive algos

--- algo/test.py
+++ algo/test.py
@@ -1,3 +1,17 @@
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Movement import *
+from Unit import *
+from HexagridHelper import *
 class test:
-    def getAction(self, a):
-        return a[0] + a[1]
+    def getAction(self, pid, grids, units, memory): 
+        results = []
+        counter = 0
+        for i in units:
+            if i.GetUnitID() == pid:
+                if(counter % 2 == 0):
+                    results.append(Movement(pid, RIGHT))
+                else:
+                    results.append(Movement(pid, UPPERRIGHT))
+                counter += 1
+        return results
--- algo/test1.py
+++ algo/test1.py
@@ -1,3 +1,18 @@
-class test1:
-    def getAction(self, a):
-        return a[0] * a[1]
+import os,sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Movement import *
+from Unit import *
+from HexagridHelper import *
+class test:
+    def getAction(self, pid, grids, units, memory): 
+        results = []
+        counter = 0
+        for i in units:
+            if i.GetUnitID() == pid:
+                if(counter % 3 == 0):
+                    results.append(Movement(pid, LEFT))
+                else:
+                    results.append(Movement(pid, DOWNLEFT))
+                counter += 1
+        return results
+        
