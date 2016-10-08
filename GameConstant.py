0da2546b95d82bf21d24ef1b482fc439e8bbb0dc
cacad101 <gbbpentium@gmail.com>
2016-10-08 16:46:16 +0800

    GameConstant

---
+++ GameConstant.py
@@ -0,0 +1,18 @@
+# Game Constant
+
+class GameConstant:
+
+    # Grid Type
+    FOW = -1
+    EMPTY = 0
+    WALL = 1
+    HIVE = 2
+
+    # move direction
+    I = 0
+    R = 1
+    DR = 2
+    DL = 3
+    L = 4
+    UL = 5
+    UR = 6
