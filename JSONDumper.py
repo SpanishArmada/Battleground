d51cac259be4aa838921af7c13e5796826d1a2b2
VotBear <vot1_bear@yahoo.com>
2016-10-09 00:38:12 +0800

    Better comments for JSONDumper

--- JSONDumper.py
+++ JSONDumper.py
@@ -1,24 +1,33 @@
 import json
 
+
+# JSON Format:
+#   root: dictionary of map and turn data
+#       map: 2D([row][col]) list of integer
+#       turn data: list (index = turn no) of turn details
+#           turn details: dict(), consists of unitData and baseData
+#               unitData: list of unit details
+#                   unitDetails: [row, col, owner ID]
+#               baseData: list of hive details
+#                   hiveDetails: [row, col, owner ID] (-1 for no owner)
+#
+#   example:
+#   {
+#       map: [[0,0,0,0][0,1,1,0][9,0,0,9]]
+#       turnData:
+#           [
+#               {
+#                   unitData: [[0,0,1], [2,2,5], [4,0,0]]
+#                   baseData: [[5,5,0], [10,5,1], [9,9,-1]]
+#               },
+#               {
+#                   unitData: [[0,1,1], [2,3,5], [4,1,0]]
+#                   baseData: [[5,5,0], [10,5,1], [9,9,-1]]
+#               },
+#           ]
+#   }
+
 class JSONDumper:
-
-    # JSON Format:
-    # root: dictionary of init + turn data
-    # {
-    #   map : 2D list of integer
-    #   int : dictionary of data per turn
-    #       {
-    #           unitData : list of unit details
-    #               [
-    #                   [row, col, owner player ID]
-    #               ]
-    #           baseData : list of hive details
-    #               [
-    #                   [row, col, owner player ID] - -1 for no owner
-    #               ]
-    #       }
-    # }
-
     def __init__(self):
         self.data = dict()
 
