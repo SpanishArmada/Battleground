097620345eba5c344e3b3788f9de737253ff7d91
cacad101 <gbbpentium@gmail.com>
2016-10-09 05:48:38 +0800

    bug fix

--- GameEngine.py
+++ GameEngine.py
@@ -9,6 +9,7 @@ import HexagridHelper as Helper
 import copy as copy
 from os.path import isfile, join, dirname, splitext, abspath, split
 import imp, importlib
+import sys
 class GameEngine:
 
     # Init function
@@ -168,7 +169,7 @@ class GameEngine:
                 self.playerMovements[i] = []
                 for key, unit in self.unitDictionary.items():
                     if(key == i):
-                        self.playerMovements[i].append(Movement(unit.GetUnitID(),C.IDLE))
+                        self.playerMovements[i].append(Movement(unit.GetUnitID(),Helper.IDLE))
 
     # Movement phase, handles all the outputs of players' scripts and moves units concurrently
     # Any collision will cause all units in the tile to die
@@ -219,10 +220,11 @@ class GameEngine:
             nearbyCoordinates = Helper.GetAllWithinDistance(r,c,C.ATTACK_RANGE)
             enemyCount = 0
             for coor in nearbyCoordinates:
-                otherUnit = self.gridUnits[coor[0]][coor[1]]
-                if (otherUnit != C.EMPTY and otherUnit.GetPlayerID() != unit.GetPlayerID()):
-                    # print(r,c,unit.GetPlayerID(),"VS",coor[0],coor[1],otherUnit.GetPlayerID())
-                    enemyCount+=1
+                if (self.IsValidCoordinate(coor[0], coor[1])):
+                    otherUnit = self.gridUnits[coor[0]][coor[1]]
+                    if (otherUnit != C.EMPTY and otherUnit.GetPlayerID() != unit.GetPlayerID()):
+                        # print(r,c,unit.GetPlayerID(),"VS",coor[0],coor[1],otherUnit.GetPlayerID())
+                        enemyCount+=1
             self.gridUnitEnemyScore[r][c] = enemyCount
 
     # If a unit has enemies near him, check the enemy score of those enemies as well
