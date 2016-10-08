bf315c33fc1de3827344604c5c641c5a6debd8f3
Dian Bakti <dbakti1605@gmail.com>
2016-10-09 06:29:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from os.path import isfile, join, dirname, splitext, abspath, split
 import imp, importlib
 from GameEngine import GameEngine
 from MapGenerator import *
+define("port", default=8888, help="run on the given port", type=int)
 
 clients = []
 algoList = []
