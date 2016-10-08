cf5082e3e764ce4e09f7ac4a3b308078a4e5ee60
Dian Bakti <dbakti1605@gmail.com>
2016-10-09 00:11:04 +0800

    added new obstacle

--- MapGenerator.py
+++ MapGenerator.py
@@ -1,7 +1,20 @@
 from GameConstant import GameConstant as C
 
-def addHive(grids, row, col, playerId):
+def addHive(grids, row, col, playerId, fort):
     grids[row][col] = C.PLAYER_HIVE + playerId
+    if(fort):
+        top  = row - 3
+        bot = row + 3
+        left = col - 3
+        right = col + 3
+        for i in range(left, right + 1):
+            grids[top][i] = C.WALL
+            grids[bot][i] = C.WALL
+        for i in range(top, bot + 1):
+            grids[i][right] = C.WALL
+            grids[i][left] = C.WALL
+        grids[top][col] = grids[bot][col] = C.NON_WALL
+        grids[row][right] = grids[row][left] = C.NON_WALL
 
 
 def getPercentile(start, end, distance):
@@ -109,16 +122,34 @@ def addRiver(grids, rowStart, rowEnd, colStart, colEnd, bridge):
 
 def getMap(row, col):
     grids = [[C.NON_WALL for i in range(row)] for j in range(col)]
-    addHive(grids, getPercentile(0, row, 0.5), 2, 0)    #player 0
-    addHive(grids, getPercentile(0, row, 0.5), col - 3) #player 1
-    addHive(grids, 5, 10, -1)                           #not taken
-    addHive(grids, row - 5, 10, -1)                     #not taken
-    addHive(grids, 5, col - 10, -1)                     #not taken
-    addHive(grids, row - 5, col - 10, -1)               #not taken
+    addHive(grids, getPercentile(0, row, 0.5), 2, 0, False)    #player 0
+    addHive(grids, getPercentile(0, row, 0.5), col - 3, 1, False) #player 1
+    addHive(grids, 5, 10, -1, False)                           #not taken
+    addHive(grids, row - 5, 10, -1, False)                     #not taken
+    addHive(grids, 5, col - 10, -1, False)                     #not taken
+    addHive(grids, row - 5, col - 10, -1, False)               #not taken
     addType4(grids, row, col, 5)
     addRiver(grids, 0, row - 1, getPercentile(0, col, 0.5) - 2, getPercentile(0, col, 0.5) + 2, 5)
-    addType3(grids, 0, 7, 7, 12, 1)
-    addType3(grids, row - 8, row - 2, col - 14, col - 8, 1)
+    addType3(grids, 0, getPercentile(0, row, 0.15), getPercentile(0, col, 0.15), getPercentile(0, col, 0.25), 1)
+    addType3(grids, getPercentile(0, row, 0.85), row - 2, getPercentile(0, col, 0.75), getPercentile(0, col, 0.85), 1)
+    addType1(grids, getPercentile(0, row, 0.7), getPercentile(0, row, 0.85), getPercentile(0, col, 0.2), getPercentile(0, col, 0.35))
+    addType1(grids, getPercentile(0, row, 0.15), getPercentile(0, row, 0.3), getPercentile(0, col, 0.65), getPercentile(0, col, 0.8))
+    addType2(grids, getPercentile(0, row, 0.3), getPercentile(0, row, 0.4), getPercentile(0, col, 0.8), getPercentile(0, col, 0.9))
+    addType2(grids, getPercentile(0, row, 0.6), getPercentile(0, row, 0.7), getPercentile(0, col, 0.1), getPercentile(0, col, 0.2))
+    return grids
+
+def getMap2(row, col):
+    grids = [[C.NON_WALL for i in range(row)] for j in range(col)]
+    addHive(grids, getPercentile(0, row, 0.5), 2, 0, False)    #player 0
+    addHive(grids, getPercentile(0, row, 0.5), col - 3, 1, False) #player 1
+    addHive(grids, getPercentile(0, row, 0.2), getPercentile(0, col, 0.2), -1, False)  #not taken
+    addHive(grids, getPercentile(0, row, 0.8), getPercentile(0, col, 0.8), -1, False)  #not taken
+    addHive(grids, getPercentile(0, row, 0.2), getPercentile(0, col, 0.8), -1, False)  #not taken
+    addHive(grids, getPercentile(0, row, 0.8), getPercentile(0, col, 0.2), -1, False)  #not taken
+    addType4(grids, row, col, 5)
+    addRiver(grids, 0, row - 1, getPercentile(0, col, 0.5) - 2, getPercentile(0, col, 0.5) + 2, 5)
+    addType3(grids, 0, getPercentile(0, row, 0.15), getPercentile(0, col, 0.15), getPercentile(0, col, 0.25), 1)
+    addType3(grids, getPercentile(0, row, 0.85), row - 2, getPercentile(0, col, 0.75), getPercentile(0, col, 0.85), 1)
     addType1(grids, getPercentile(0, row, 0.7), getPercentile(0, row, 0.85), getPercentile(0, col, 0.2), getPercentile(0, col, 0.35))
     addType1(grids, getPercentile(0, row, 0.15), getPercentile(0, row, 0.3), getPercentile(0, col, 0.65), getPercentile(0, col, 0.8))
     addType2(grids, getPercentile(0, row, 0.3), getPercentile(0, row, 0.4), getPercentile(0, col, 0.8), getPercentile(0, col, 0.9))
