ac8da44656c28266ece42192474cbb0f5c352eb8
VotBear <vot1_bear@yahoo.com>
2016-10-08 20:00:24 +0800

    Import changes to gameengine

--- GameEngine.py
+++ GameEngine.py
@@ -3,16 +3,16 @@
 from Grid import Grid
 from Unit import Unit
 from Hive import Hive
-from GameConstant import GameConstant
+from GameConstant import GameConstant as C
 import HexagridHelper
 
 class GameEngine:
 
     def __init__(self, col = 50, row = 50, playerNum = 2):
-        self.gridTerrainMain = [[0 for i in range(col)] for j in range(row)]
-        self.gridUnits = [[0 for i in range(col)] for j in range(row)]
-        self.gridFoW = [[[0 for i in range(col)] for j in range(row)] for k in range(playerNum)]
-        self.playerMovements = [0 for i in range(playerNum)]
+        self.gridTerrainMain = [[C.EMPTY for i in range(col)] for j in range(row)]
+        self.gridUnits = [[C.EMPTY for i in range(col)] for j in range(row)]
+        self.gridFoW = [[[C.EMPTY for i in range(col)] for j in range(row)] for k in range(playerNum)]
+        self.playerMovements = [C.EMPTY for i in range(playerNum)]
         self.playerNum = playerNum
         self.row = row
         self.col = col
@@ -42,6 +42,14 @@ class GameEngine:
         # Calculate enemy
         # check death
         # apply death
+
+    def CalculateEnemyScore(self):
+        for r in range(self.row):
+            for c in range(self.col):
+                pass
+
+    def IsValidCoordinate(self, row, col):
+        return (row>=0 and row < self.row and col>=0 and col<self.col)
 
     def writeToFile(self):
         pass
