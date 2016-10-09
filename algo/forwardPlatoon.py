ba9b2d3d2a587f820ce68c92efb26b8a448d7437
VotBear <vot1_bear@yahoo.com>
2016-10-09 15:02:48 +0800

    ForwardPlatoon

--- algo/forwardPlatoon.py
+++ algo/forwardPlatoon.py
@@ -16,23 +16,82 @@ class forwardPlatoon:
         self.isOccupied = [[False for i in range(self.maxcol)] for j in range(self.maxrow)]
         results = []
         counter = 0
+
         for i in range(self.maxcol):
             for j in range(self.maxrow):
                 if (isinstance(units[j][i], int)):
                     continue
                 if units[j][i].GetPlayerID() == pid:
-                    if (self.isOccupied[j][i] or grids[j][i]>=9):
+                    counter += 1
+
+        if (counter<10):
+            for i in range(self.maxcol):
+                for j in range(self.maxrow):
+                    if (isinstance(units[j][i], int)):
+                        continue
+                    if units[j][i].GetPlayerID() == pid:
+                        if (self.isOccupied[j][i] or grids[j][i]>=9):
+                            movrig = GetMoveTarget(j,i,RIGHT)
+                            movuprig = GetMoveTarget(j,i,UPPERRIGHT)
+                            movdowrig = GetMoveTarget(j,i,DOWNRIGHT)
+                            if (j < self.centerRow): #above middle
+                                if (self.isValidMove(movdowrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                                    self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                                elif (self.isValidMove(movrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                                    self.isOccupied[movrig[0]][movrig[1]] = True
+                                elif (self.isValidMove(movuprig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                                    self.isOccupied[movuprig[0]][movuprig[1]] = True
+                                else:
+                                    results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                                    self.isOccupied[j][i] = True
+
+                            elif (j > self.centerRow): #below middle
+                                if (self.isValidMove(movuprig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                                    self.isOccupied[movuprig[0]][movuprig[1]] = True
+                                elif (self.isValidMove(movrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                                    self.isOccupied[movrig[0]][movrig[1]] = True
+                                elif (self.isValidMove(movdowrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                                    self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                                else:
+                                    results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                                    self.isOccupied[j][i] = True
+
+                            else: # center
+                                if (self.isValidMove(movrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                                    self.isOccupied[movrig[0]][movrig[1]] = True
+                                elif (self.isValidMove(movuprig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                                    self.isOccupied[movuprig[0]][movuprig[1]] = True
+                                elif (self.isValidMove(movdowrig)):
+                                    results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                                    self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                                else:
+                                    results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                                    self.isOccupied[j][i] = True
+        else :
+            for i in range(self.maxcol-1,-1,-1):
+                for j in range(self.maxrow):
+                    if (isinstance(units[j][i], int)):
+                        continue
+                    if units[j][i].GetPlayerID() == pid:
                         movrig = GetMoveTarget(j,i,RIGHT)
                         movuprig = GetMoveTarget(j,i,UPPERRIGHT)
                         movdowrig = GetMoveTarget(j,i,DOWNRIGHT)
                         if (j < self.centerRow): #above middle
-                            if (self.isValidMove(movdowrig)):
+                            if (self.isValidMove(movdowrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
                                 self.isOccupied[movdowrig[0]][movdowrig[1]] = True
-                            elif (self.isValidMove(movrig)):
+                            elif (self.isValidMove(movrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), RIGHT))
                                 self.isOccupied[movrig[0]][movrig[1]] = True
-                            elif (self.isValidMove(movuprig)):
+                            elif (self.isValidMove(movuprig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
                                 self.isOccupied[movuprig[0]][movuprig[1]] = True
                             else:
@@ -40,13 +99,13 @@ class forwardPlatoon:
                                 self.isOccupied[j][i] = True
 
                         elif (j > self.centerRow): #below middle
-                            if (self.isValidMove(movuprig)):
+                            if (self.isValidMove(movuprig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
                                 self.isOccupied[movuprig[0]][movuprig[1]] = True
-                            elif (self.isValidMove(movrig)):
+                            elif (self.isValidMove(movrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), RIGHT))
                                 self.isOccupied[movrig[0]][movrig[1]] = True
-                            elif (self.isValidMove(movdowrig)):
+                            elif (self.isValidMove(movdowrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
                                 self.isOccupied[movdowrig[0]][movdowrig[1]] = True
                             else:
@@ -54,13 +113,13 @@ class forwardPlatoon:
                                 self.isOccupied[j][i] = True
 
                         else: # center
-                            if (self.isValidMove(movrig)):
+                            if (self.isValidMove(movrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), RIGHT))
                                 self.isOccupied[movrig[0]][movrig[1]] = True
-                            elif (self.isValidMove(movuprig)):
+                            elif (self.isValidMove(movuprig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
                                 self.isOccupied[movuprig[0]][movuprig[1]] = True
-                            elif (self.isValidMove(movdowrig)):
+                            elif (self.isValidMove(movdowrig, False)):
                                 results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
                                 self.isOccupied[movdowrig[0]][movdowrig[1]] = True
                             else:
@@ -68,12 +127,12 @@ class forwardPlatoon:
                                 self.isOccupied[j][i] = True
         return results
 
-    def isValidMove(self,coor):
+    def isValidMove(self,coor, ignoreAlly = True):
         if (coor[0]<0 or coor[0]>=self.maxrow or coor[1]<0 or coor[1]>=self.maxcol):
             return False
         if (self.grids[coor[0]][coor[1]] == 1):
             return False
-        if (not isinstance(self.units[coor[0]][coor[1]], int)):
+        if (not ignoreAlly and not isinstance(self.units[coor[0]][coor[1]], int)):
             if (self.units[coor[0]][coor[1]].GetPlayerID() == self.pid):
                 return False
         return not self.isOccupied[coor[0]][coor[1]]
