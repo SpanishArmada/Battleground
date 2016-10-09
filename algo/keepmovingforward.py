795f995184448b714fa62172ecd09a99da41d5af
VotBear <vot1_bear@yahoo.com>
2016-10-09 13:19:05 +0800

    movingforwardfix

--- algo/keepmovingforward.py
+++ algo/keepmovingforward.py
@@ -11,6 +11,7 @@ class keepmovingforward:
         self.grids = grids
         self.units = units
         self.maxrow = len(units)
+        self.centerRow = self.maxrow//2
         self.maxcol = len(units[0])
         self.isOccupied = [[False for i in range(self.maxcol)] for j in range(self.maxrow)]
         results = []
@@ -23,22 +24,51 @@ class keepmovingforward:
                     movrig = GetMoveTarget(j,i,RIGHT)
                     movuprig = GetMoveTarget(j,i,UPPERRIGHT)
                     movdowrig = GetMoveTarget(j,i,DOWNRIGHT)
-                    if (self.isValidMove(movrig)):
-                        results.append(Movement(units[j][i].GetUnitID(), RIGHT))
-                        self.isOccupied[movrig[0]][movrig[1]] = True
-                    elif (self.isValidMove(movuprig)):
-                        results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
-                        self.isOccupied[movuprig[0]][movuprig[1]] = True
-                    elif (self.isValidMove(movdowrig)):
-                        results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
-                        self.isOccupied[movdowrig[0]][movdowrig[1]] = True
-                    else:
-                        results.append(Movement(units[j][i].GetUnitID(), IDLE))
-                        self.isOccupied[j][i] = True
+                    if (j < self.centerRow): #above middle
+                        if (self.isValidMove(movdowrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                            self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                        elif (self.isValidMove(movrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                            self.isOccupied[movrig[0]][movrig[1]] = True
+                        elif (self.isValidMove(movuprig)):
+                            results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                            self.isOccupied[movuprig[0]][movuprig[1]] = True
+                        else:
+                            results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                            self.isOccupied[j][i] = True
+
+                    elif (j > self.centerRow): #below middle
+                        if (self.isValidMove(movuprig)):
+                            results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                            self.isOccupied[movuprig[0]][movuprig[1]] = True
+                        elif (self.isValidMove(movrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                            self.isOccupied[movrig[0]][movrig[1]] = True
+                        elif (self.isValidMove(movdowrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                            self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                        else:
+                            results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                            self.isOccupied[j][i] = True
+
+                    else: # center
+                        if (self.isValidMove(movrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), RIGHT))
+                            self.isOccupied[movrig[0]][movrig[1]] = True
+                        elif (self.isValidMove(movuprig)):
+                            results.append(Movement(units[j][i].GetUnitID(), UPPERRIGHT))
+                            self.isOccupied[movuprig[0]][movuprig[1]] = True
+                        elif (self.isValidMove(movdowrig)):
+                            results.append(Movement(units[j][i].GetUnitID(), DOWNRIGHT))
+                            self.isOccupied[movdowrig[0]][movdowrig[1]] = True
+                        else:
+                            results.append(Movement(units[j][i].GetUnitID(), IDLE))
+                            self.isOccupied[j][i] = True
         return results
 
     def isValidMove(self,coor):
-        if (coor[0]<0 or coor[0]>=self.maxrow or coor[1]<0 or coor[1]>self.maxcol):
+        if (coor[0]<0 or coor[0]>=self.maxrow or coor[1]<0 or coor[1]>=self.maxcol):
             return False
         if (self.grids[coor[0]][coor[1]] == 1):
             return False
